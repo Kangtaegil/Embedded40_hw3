88d5421e08d6092f0173862bb65e5505598e42a6
Kangtaegil <dmsrkql_8@naver.com>
2018-04-30 15:48:21 +0000

    git add ReadME.txt

---
+++ ReadME.txt
@@ -0,0 +1,3 @@
+youtube ULR : https://youtu.be/45PABQdxVBI
+
+I changed the humidity reference point from 70 percent to 50 percent.
---
+++ detect_humidity.c
@@ -0,0 +1,162 @@
+/*
+ *      dht22.c:
+ *	Simple test program to test the wiringPi functions
+ *	Based on the existing dht11.c
+ *	Amended by technion@lolware.net
+ */
+
+#include <wiringPi.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <softPwm.h>
+
+//#include "locking.h"
+
+#define MAXTIMINGS 85
+#define FAN 22
+#define MOTOR 23
+#define RED 7
+
+int ret_humid, ret_temp;
+
+//static int DHTPIN = 7;
+static int DHTPIN = 11;
+static int dht22_dat[5] = {0,0,0,0,0};
+
+void sig_handler(int signo){
+	printf("Process Stop\n");
+	digitalWrite(FAN, 0);
+	digitalWrite(MOTOR, 0);
+	digitalWrite(RED, 0);
+	exit(0);
+}
+
+static uint8_t sizecvt(const int read)
+{
+  /* digitalRead() and friends from wiringpi are defined as returning a value
+  < 256. However, they are returned as int() types. This is a safety function */
+
+  if (read > 255 || read < 0)
+  {
+    printf("Invalid data from wiringPi library\n");
+    exit(EXIT_FAILURE);
+  }
+  return (uint8_t)read;
+}
+
+int read_dht22_dat()
+{
+  uint8_t laststate = HIGH;
+  uint8_t counter = 0;
+  uint8_t j = 0, i;
+
+  dht22_dat[0] = dht22_dat[1] = dht22_dat[2] = dht22_dat[3] = dht22_dat[4] = 0;
+
+  // pull pin down for 18 milliseconds
+  pinMode(DHTPIN, OUTPUT);
+  digitalWrite(DHTPIN, HIGH);
+  delay(10);
+  digitalWrite(DHTPIN, LOW);
+  delay(18);
+  // then pull it up for 40 microseconds
+  digitalWrite(DHTPIN, HIGH);
+  delayMicroseconds(40); 
+  // prepare to read the pin
+  pinMode(DHTPIN, INPUT);
+
+  // detect change and read data
+  for ( i=0; i< MAXTIMINGS; i++) {
+    counter = 0;
+    while (sizecvt(digitalRead(DHTPIN)) == laststate) {
+      counter++;
+      delayMicroseconds(1);
+      if (counter == 255) {
+        break;
+      }
+    }
+    laststate = sizecvt(digitalRead(DHTPIN));
+
+    if (counter == 255) break;
+
+    if ((i >= 4) && (i%2 == 0)) {
+      dht22_dat[j/8] <<= 1;
+      if (counter > 50)
+        dht22_dat[j/8] |= 1;
+      j++;
+    }
+  }
+
+  if ((j >= 40) && (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
+        float t, h;
+
+        h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
+        h /= 10;
+        t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
+        t /= 10.0;
+
+        if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
+
+	ret_humid = (int)h;
+	ret_temp = (int)t;
+	return ret_humid;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int main (void)
+{
+	int received_humid;
+
+	DHTPIN = 11;
+
+	if (wiringPiSetup() == -1)
+		exit(EXIT_FAILURE) ;
+
+	if (setuid(getuid()) < 0)
+	{
+		perror("Dropping privileges failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	pinMode(FAN, OUTPUT);
+	pinMode(MOTOR, OUTPUT);
+	pinMode(RED, OUTPUT);
+
+	while (read_dht22_dat() == 0)
+	{
+		delay(500); // wait 1sec to refresh
+	}
+
+	while(1){
+		if(read_dht22_dat() == 0)
+			continue;
+
+		else{
+			received_humid = ret_humid;
+			printf("Humidity = %d\n", received_humid);
+			delay(500);
+			if(received_humid >= 40){
+				printf("Fan,Motor,LED ON!\n");
+				digitalWrite(FAN, 1);
+				digitalWrite(MOTOR, 1);
+				digitalWrite(RED, 1);
+				delay(3000);
+				digitalWrite(FAN, 0);
+				digitalWrite(MOTOR, 0);
+				digitalWrite(RED, 0);
+				delay(500);
+			}
+		}
+	}
+
+	return 0;
+}
